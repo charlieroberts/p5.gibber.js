0163bb8e987a4de259c56da2bcac5db7e89eec0c
charlieroberts <charlie@charlie-roberts.com>
2014-09-02 18:56:14 -0700

    new directory structure for examples. minified version

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var browserify = require( 'browserify' ),
     gulp = require( 'gulp' ),
     source = require( 'vinyl-source-stream' ),
     buffer = require( 'vinyl-buffer' ),
+    rename = require( 'gulp-rename' ),    
     uglify = require( 'gulp-uglify' );
 
 gulp.task( 'default', function(){
@@ -11,9 +12,11 @@ gulp.task( 'default', function(){
 
   out.bundle()
     .pipe( source('p5.gibber.js') )
+    .pipe( gulp.dest('./dist/') )
     .pipe( buffer() )
-    //.pipe( uglify() )
-    .pipe( gulp.dest('./dist/') );
+    .pipe( uglify() )
+    .pipe( rename('p5.gibber.min.js') )
+    .pipe( gulp.dest('./dist/') )
 
   return out
 });
